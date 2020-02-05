1fdb88e5fc81aa84d61b50fe94cceb48a53399ca
messaouda83 <messaoudabenchikh@yahoo.fr>
2020-02-05 14:53:49 +0100

    ajout des classe film.php artiste.php genre.php

---
+++ Models/Artiste.php
@@ -0,0 +1,29 @@
+<?php 
+
+class Artiste {
+    private $nom_artiste;
+    private $prenom_artiste;
+    private $date_naissance;
+
+    public function __construct($nom_artiste, $prenom_artiste, $date_naissance)
+    {
+
+
+    }
+    public function createArtiste()
+    {
+
+
+
+    }
+
+    public function deleteArtiste()
+    {
+
+
+    }
+    public function updateArtiste(){
+
+
+    }
+}
---
+++ Models/Genre.php
@@ -0,0 +1,24 @@
+<?php 
+
+class Genre {
+        private $nom_genre;
+        public function __construct($nom_genre){
+
+            
+        }
+        public function createGenre(){
+
+            
+
+        }
+         public function deleteGenre(){
+
+            
+        }
+        public function updateGenre(){
+
+            
+        }
+
+
+        }
