9f9a9ae89ecc7755a51da2f20460be3b85cc9fb6
messaouda83 <messaoudabenchikh@yahoo.fr>
2020-02-11 13:44:43 +0100

    modifier et ajouter la fonction insertNewArtist dans le fichier Models/Artist.php

--- Models/Artist.php
+++ Models/Artist.php
@@ -4,18 +4,26 @@ class Artist extends BDDConnect
 {
     public function __construct()
     {
-        $this->pdo = parent::getPdo();
+        self::$_pdo = parent::getPdo();
     }
 
     public function getAllArtists()
     {
         $sql = 'SELECT * FROM artists';
-        $req = $this->pdo->prepare($sql);
+        $req =  self::$_pdo->prepare($sql);
         $req->execute();
         return $req->fetchAll();
     }
 
-    
+    public function insertArtist($lastname_artist, $firstname_artist, $birth_date)
+        {
+    $sql= "INSERT INTO artists(lastname_artist, firstname_artist, birth_date) VALUES (:lastname_artist, :firstname_artist, :birth_date)" ;
+    $req=self::$_pdo->prepare($sql);
+    $req->bindParam(':lastname_artist', $lastname_artist);
+    $req-> bindParam(':firstname_artist', $firstname_artist);
+    $req-> bindParam(':birth_date', $birth_date);
+    return $req->execute();
+        }
 
     
 }
