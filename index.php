524f7870126f3f898c424cc2edb6403a3ab9f705
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-02-13 10:59:25 +0100

    Added all routes for admin pages

--- index.php
+++ index.php
@@ -3,6 +3,22 @@ require_once 'vendor/autoload.php';
 
 $mainRouter = new Router($_GET['uri']);
 
+$mainRouter->addRouteGET('/admin/artist/get/:id', 'Admin.getArtistData');
+
+$mainRouter->addRouteGET('/admin/genre/add', 'Admin.showAddGenre');
+$mainRouter->addRouteGET('/admin/genre/edit', 'Admin.showEditGenre');
+$mainRouter->addRouteGET('/admin/genre/delete', 'Admin.showDelGenre');
+
+$mainRouter->addRouteGET('/admin/artist/add', 'Admin.showAddArtist');
+$mainRouter->addRouteGET('/admin/artist/edit', 'Admin.showEditArtist');
+$mainRouter->addRouteGET('/admin/artist/delete', 'Admin.showDelArtist');
+
+$mainRouter->addRouteGET('/admin/movie/add', 'Admin.showAddMovie');
+$mainRouter->addRouteGET('/admin/movie/edit', 'Admin.showEditMovie');
+$mainRouter->addRouteGET('/admin/movie/delete', 'Admin.showDelMovie');
+
+$mainRouter->addRouteGET('/login', 'Admin.showLogin');
+$mainRouter->addRouteGET('/admin', 'Admin.showAdmin');
 $mainRouter->addRouteGET('/', 'Home.showHome');
 
 
