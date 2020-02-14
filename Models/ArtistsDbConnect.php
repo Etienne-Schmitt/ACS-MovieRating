d2b3682fba6553cbf59689cfa1454fafe65f2b11
Mineli <monte.nemi@gmail.com>
2020-02-14 16:12:46 +0100

    added indentation

--- Models/ArtistsDbConnect.php
+++ Models/ArtistsDbConnect.php
@@ -1,16 +1,18 @@
 <?php
 
-class ArtistsDbConnect extends dbConnect {
-    public function __construct()
-    {
-        $this->pdo = parent::getPdo();
-    }
+class ArtistsDbConnect extends dbConnect
+{
+  public function __construct()
+  {
+    $this->pdo = parent::getPdo();
+  }
 
-    public function getAllArtists() {
-        $req = $this->pdo->prepare(
-          'SELECT * FROM artiste'
-        );
-        $req->execute();
-        return $req->fetchAll();
-    }
+  public function getAllArtists()
+  {
+    $req = $this->pdo->prepare(
+      'SELECT * FROM artiste'
+    );
+    $req->execute();
+    return $req->fetchAll();
+  }
 }
