0cd0743a8aaa500e7be1601d405605a7b4bcf5ff
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-02-14 11:41:44 +0100

    Format and comment all method

--- Controllers/MoviesController.php
+++ Controllers/MoviesController.php
@@ -5,104 +5,160 @@ use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 
-
 /**
  * Class MoviesController
- * @mixin
  */
 class MoviesController extends Controller
 {
-  /**
-   * MoviesController constructor
-   */
-  public function __construct()
-  {
-    parent::__construct();
-    $this->model = new MoviesModel();
+    /** @var MoviesDbConnect */
+    private $dbLink;
 
-    self::$_twig = parent::getTwig();
-  }
-
-    public function index() {
-      $result = $this->model->getAllMovies();
-      $pageTwig = 'Movies/allMovies.html.twig';
-      $template = self::$_twig->load($pageTwig);
-      echo $template->render(["result" => $result]);
+    /**
+     * MoviesController constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->dbLink = new MoviesDbConnect();
+        self::$_twig = parent::getTwig();
     }
 
-    public function showMovie(int $id) {
-      $actorsDetails = $this->model->getActorsDetails($id);
-      $movieDetails = $this->model->getMovieDetails($id);
-      $pageTwig = 'Movies/showMovie.html.twig';
-      //self::$_twig->addGlobal('actor', $result);
-      $template = self::$_twig->load($pageTwig);
-      echo $template->render(["actorsDetails" => $actorsDetails, "movieDetails" => $movieDetails]);
+    /**
+     * @param string $page page name without .html.twig that will be displayed
+     * @return string Twig Rendered page
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    private function showPage($page)
+    {
+        $page .= ".html.twig";
+        $template = self::$_twig->load($page);
+        return $template->render();
     }
 
-    public function addMovie() {
-      // Fetch all directors and genres from the database into two dropdown lists on the Add Movie page
-      $directors = $this->model->getAllDirectors();
-      $genres = $this->model->getAllGenres();
-      // Display the input form on the Add Movie page
-      $pageTwig = 'Movies/addMovie.html.twig';
-      $template = self::$_twig->load($pageTwig);
-      echo $template->render(["directors" => $directors, "genres" => $genres]);
+    /**
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function showMovieHome()
+    {
+        $moviesData = $this->dbLink->getAllMoviesData();
+        self::$_twig->addGlobal('moviesArray', $moviesData);
+        echo $this->showPage("list.movie.home.html.twig");
+
     }
 
-    public function insertNewMovie() {
-      $titre = $_POST['titre'];
-      $annee_sortie = $_POST['annee_sortie'];
-      $affiche = $_FILES['affiche']['name'];
-      $affiche_temp = $_FILES['affiche']['tmp_name'];
-      $synopsis = $_POST['synopsis'];
-      $genre = $_POST['genre'];
-      $director = $_POST['director'];
-
-      move_uploaded_file($affiche_temp, "../Uploads/posters/$affiche");
-
-      $this->model->insertMovie($titre, $annee_sortie, $affiche, $synopsis, $genre, $director);
-
-      header("Location: http://localhost/ACS-MovieRating/movies");
+    /**
+     * @param int $id
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function showMovie($id)
+    {
+        $allActorsData = $this->dbLink->getActorsDetails($id);
+        $movieData = $this->dbLink->getMovieDetails($id);
+        self::$_twig->addGlobal('actorsArray', $allActorsData);
+        self::$_twig->addGlobal('movie', $movieData);
+        echo $this->showPage("Movies/specific.movie");
     }
 
-    public function getAllMovies() {
-      $getAllMovies = $this->model->getAllMovies();
-      $pageTwig = 'Movies/getAllMovies.html.twig';
-      $template = self::$_twig->load($pageTwig);
-      echo $template->render(["getAllMovies" => $getAllMovies]);
+    /**
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function getAllMovies()
+    {
+        $getAllMovies = $this->dbLink->getAllMoviesData();
+
+        self::$_twig->addGlobal('getAllMovies', $getAllMovies);
+
+        echo $this->showPage("Movies/getAllMovies");
     }
 
-    // Function to fetch all the data from the DB into the Update Movie form on the Update Movie page
-    public function getMovie($id_film) {
-      $movieDetails = $this->model->getMovieDetails($id_film);
-      $director = $this->model->getDirectorDetails($id_film);
-      $directors = $this->model->getAllOtherDirectors($id_film);
+    /**
+     * Deprecated only method down there
+     */
 
-      $genre = $this->model->getGenre($id_film);
-      $genres = $this->model->getAllOtherGenres($id_film);
+    /**
+     * Add a new Movie to the DB
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @deprecated Only for test envs, done in prod via admin pages
+     */
+    public function addMovie()
+    {
+        self::$_twig->addGlobal('directors', $this->dbLink->getAllDirectors());
+        self::$_twig->addGlobal('genres', $this->dbLink->getAllGenres());
 
-      $pageTwig = 'Movies/getMovie.html.twig';
-      $template = self::$_twig->load($pageTwig);
-      echo $template->render(["movieDetails" => $movieDetails, 'director' => $director, 'directors' => $directors, 'genre' => $genre, 'genres' => $genres]);
+        echo $this->showPage("Movies/addMovie");
     }
 
-    public function updateMovie($id_film) {
-      $titre = $_POST['titre'];
-      $annee_sortie = $_POST['annee_sortie'];
-      $affiche = $_FILES['affiche']['name'];
-      $affiche_temp = $_FILES['affiche']['tmp_name'];
-      $synopsis = $_POST['synopsis'];
-      $genre = $_POST['genre'];
-      $director = $_POST['director'];
+    /**
+     * Fetch all data from DB into the Update Movie form on the Update Movie page
+     * @param $id_film
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @deprecated Only for test envs, done in prod via admin pages
+     */
+    public function getMovie($id_film)
+    {
+        $movieDetails = $this->dbLink->getMovieDetails($id_film);
+        $director = $this->dbLink->getDirectorDetails($id_film);
+        $directors = $this->dbLink->getAllOtherDirectors($id_film);
 
-      move_uploaded_file($affiche_temp, "../Uploads/posters/$affiche");
+        $genre = $this->dbLink->getGenre($id_film);
+        $genres = $this->dbLink->getAllOtherGenres($id_film);
 
-      if($this->model->updateMovie($id_film, $titre, $annee_sortie, $affiche, $synopsis, $genre, $director)) {
-        echo "ok";
-      } else {
-        echo "not ok";
-      };
+        $pageTwig = 'Movies/test.update.movie.twig';
+        $template = self::$_twig->load($pageTwig);
+        echo $template->render(["movieDetails" => $movieDetails, 'director' => $director, 'directors' => $directors, 'genre' => $genre, 'genres' => $genres]);
+    }
 
-      // header("Location: http://localhost/ACS-MovieRating/movies");
+    /**
+     * @deprecated Only for test envs, done in prod via admin pages
+     */
+    public function insertNewMovie()
+    {
+        $titre = $_POST['titre'];
+        $annee_sortie = $_POST['annee_sortie'];
+        $affiche = $_FILES['affiche']['name'];
+        $affiche_temp = $_FILES['affiche']['tmp_name'];
+        $synopsis = $_POST['synopsis'];
+        $genre = $_POST['genre'];
+        $director = $_POST['director'];
+
+        move_uploaded_file($affiche_temp, "../Uploads/posters/$affiche");
+
+        $this->dbLink->insertMovie($titre, $annee_sortie, $affiche, $synopsis, $genre, $director);
+
+        header("Location: http://localhost/ACS-MovieRating/movies");
+    }
+
+    /**
+     * @deprecated Only for test envs, done in prod via admin pages
+     */
+    public function updateMovie($id_film)
+    {
+        $titre = $_POST['titre'];
+        $annee_sortie = $_POST['annee_sortie'];
+        $affiche = $_FILES['affiche']['name'];
+        $affiche_temp = $_FILES['affiche']['tmp_name'];
+        $synopsis = $_POST['synopsis'];
+        $genre = $_POST['genre'];
+        $director = $_POST['director'];
+
+        move_uploaded_file($affiche_temp, "../Uploads/posters/$affiche");
+
+        if ($this->dbLink->updateMovie($id_film, $titre, $annee_sortie, $affiche, $synopsis, $genre, $director)) {
+            echo "ok";
+        } else {
+            echo "not ok";
+        }
     }
 }
