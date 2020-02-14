328b4163c191c1751c70fb7e6690c47949ac185a
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-02-14 15:06:10 +0100

    ajout de 2 function getArtistById() et updateArtistById()

--- Controllers/ArtistController.php
+++ Controllers/ArtistController.php
@@ -3,12 +3,12 @@ class ArtistController extends Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->movieArtist = new Artist();
+        $this->Artist = new Artist();
     }
 
-    public function showArtist() {
-        $result = $this->movieArtist->getAllArtists();
-        //var_dump($result);
+    public function showAllArtists() {
+        $result = $this->Artist->getAllArtists();
+      
         $pageTwig = 'artist/artist.html.twig';
         $template = self::$_twig->load($pageTwig);
         echo $template->render(["result" => $result]);
@@ -22,9 +22,35 @@ class ArtistController extends Controller {
         $lastname_artist = $_POST['lastname_artist'];
         $firstname_artist = $_POST['firstname_artist'];
         $birth_date = $_POST['birth_date'];
-        $this->movieArtist ->insertArtist($lastname_artist, $firstname_artist, $birth_date);
+        $this->Artist ->insertArtist($lastname_artist, $firstname_artist, $birth_date);
         header('Location: http://localhost/MovieFilm/artist/add');
         exit;
 
+    }
+    public function getArtistById($id)
+    {
+        $result = $this->Artist->getAllArtists($id);
+        $pageTwig = 'genre/updateArtist.html.twig';
+        $template = self::$_twig->load($pageTwig);
+        echo $template->render(["result" => $result]);
+    }
+
+    public function updateArtistById($id_artist)
+    {
+        $lastname_artist = $_POST['lastname_artist'];
+        $firstname_artist = $_POST['firstname_artist'];
+        $birth_date = $_POST['birth_date'];
+        $this->Artist->updateArtist($lastname_artist, $firstname_artist, $birth_date);
+        $uri= self::$_baseUrl;
+        header("Location:$uri/artist");
+
+    }
+
+    public function deleteArtistById($id)
+    {
+        $this->movieGenre->deleteArtist($id);
+        $uri = self::$_baseUrl;
+        header("Location: $uri/artist");
+        exit ;
     }
 }
