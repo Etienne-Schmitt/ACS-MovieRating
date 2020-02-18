962f35743eac4ec251818f8fd1c8d3fc9be49b0a
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-02-18 16:40:53 +0100

    Suppresion ancienne methode et formatage dans HomeController

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -3,54 +3,21 @@
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
-use App\Entity\Genre;
 
 
 /**
  * Class HomeController
- * @mixin
  */
-   
 class HomeController extends Controller
 {
-    /**
-     * HomeController constructor.
-     * 
-     */
-    
     public function __construct()
     {
         parent::__construct();
         self::$_twig = parent::getTwig();
-        
     }
 
     public function showHome()
     {
-        echo $this->showPage('home');
+        echo $this->showPage('home.html.twig');
     }
-
-    public function showGenre()
-    {
-        echo $this->showPage('genre');
-    }
-
-    public function showActor()
-    {
-        echo $this->showPage('actor');
-    }
-
-    public function showDirector()
-    {
-        echo $this->showPage('director');
-    }
-
-    private function showPage($page)
-    {
-        $template = self::$_twig->load($page . ".html.twig" );
-        return $template->render();
-    }
-
-
-
 }
