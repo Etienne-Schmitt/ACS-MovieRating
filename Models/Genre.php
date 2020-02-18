6ecd036b8044e6b2238a0b07ffb643c2cc9de4e5
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-02-18 16:32:46 +0100

    Fixe mauvais nombre de parametres pour insertGenre() et getGenre() avec renomage de getGenre() en getGenreById()

--- Models/Genre.php
+++ Models/Genre.php
@@ -16,29 +16,19 @@ class Genre extends BDDConnect
         return $req->fetchAll();
     }
 
-    public function insertGenre($id_genre,$genre){
+    public function insertGenre($genre){
         $sql = "INSERT INTO movie_genres(genre) values (:genre)";
         $req = self::$_pdo->prepare($sql);
         $req->bindParam(':genre', $genre);
         return $req->execute();
-      
     }
     
-    public function getGenre()
+    public function getGenrebyId($id_genre)
     {
         $sql ="SELECT genre FROM movie_genres
         WHERE id_genre = $id_genre";
         $req = self::$_pdo->prepare($sql);
         return $req->execute();
-    }
-    
-    public function getGenreByName($id_genre)
-    {
-        $sql="SELECT * from movie_genres where id_genre = :id_genre";
-        $req=self::$_pdo->prepare($sql);
-        $req->bindParam(':id_genre', $id_genre, PDO::PARAM_INT);
-        $req->execute();
-        return $req->fetch(PDO::FETCH_OBJ);
     }
 
     public function updateGenre($id_genre, $genre)
@@ -56,5 +46,4 @@ class Genre extends BDDConnect
         $req = self::$_pdo->prepare($sql);
     return $req->execute();
     }
-    
-    } 
+} 
