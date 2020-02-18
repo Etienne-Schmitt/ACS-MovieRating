e4d5c5b1df660027d925c6faa18b3f39e5a61ade
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-02-18 16:58:41 +0100

    Séparation des methodes admin et users dans différentes classes

--- Models/Artist.php
+++ Models/Artist.php
@@ -1,5 +1,8 @@
-<?php 
+<?php
 
+/**
+ * Class Artist
+ */
 class Artist extends BDDConnect
 {
     public function __construct()
@@ -7,47 +10,15 @@ class Artist extends BDDConnect
         self::$_pdo = parent::getPdo();
     }
 
+    /**
+     * Get all artists id and data
+     * @return array
+     */
     public function getAllArtists()
     {
         $sql = "SELECT * FROM artists";
-        $req =  self::$_pdo->prepare($sql);
+        $req = self::$_pdo->prepare($sql);
         $req->execute();
         return $req->fetchAll();
-
-        
-
     }
-
-    public function insertArtist($lastname_artist, $firstname_artist, $birth_date)
-        {
-    $sql= "INSERT INTO artists(lastname_artist, firstname_artist, birth_date) VALUES (:lastname_artist, :firstname_artist, :birth_date)" ;
-    $req=self::$_pdo->prepare($sql);
-    $req->bindParam(':lastname_artist', $lastname_artist);
-    $req-> bindParam(':firstname_artist', $firstname_artist);
-    $req-> bindParam(':birth_date', $birth_date);
-    return $req->execute();
-        }
-
- public function updateArtist($lastname_artist, $firstname_artist, $birth_date)
-    {
-
-        $sql="UPDATE artists
-        SET lastname_artist='$lastname_artist',
-        firstname_artist='$firstname_artist',
-        birth_date='$birth_date'
-        WHERE id_artist= $id_artist";
-
-        $req=self::$_pdo->prepare($sql);
-        return $req->execute();
-       
-    }
-
-   public function deleteArtist($id_artist)
-   {
-        $sql= "DELETE FROM artists
-        WHERE id_artist= '$ad_artist' " ;
-        $req= self::$_pdo->prepare($sql);
-        return $req->execute();
-
-   } 
 }
