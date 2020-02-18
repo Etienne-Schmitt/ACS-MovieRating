ef11dc0a3cb0be2dd659f82108d34a29a1284552
messaouda BENCHIKH <57708302+messaouda83@users.noreply.github.com>
2020-02-18 16:59:08 +0100

    Séparation des methodes admin et users dans différentes classes

---
+++ Admin/Models/AdminGenre.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class AdminGenre extends Genre
+{
+    public function insertGenre($genre)
+    {
+        $sql = "INSERT INTO movie_genres (genre) values (:genre)";
+        $req = self::$_pdo->prepare($sql);
+        $req->bindParam(':genre', $genre);
+        return $req->execute();
+    }
+
+    public function updateGenre($id_genre, $genre)
+    {
+        $sql = "UPDATE movie_genres SET genre = :genre WHERE id_genre = :id";
+        $req = self::$_pdo->prepare($sql);
+        $req->bindParam(':genre', $genre);
+        $req->bindParam(':id', $id_genre);
+        return $req->execute();
+    }
+
+    public function deleteGenre($id_genre)
+    {
+        $sql = "DELETE FROM movie_genres WHERE id_genre = :id";
+        $req = self::$_pdo->prepare($sql);
+        $req->bindParam(':id', $id_genre);
+        return $req->execute();
+    }
+
+    public function getGenrebyId($id_genre)
+    {
+        $sql = "SELECT genre FROM movie_genres WHERE id_genre = :id";
+        $req = self::$_pdo->prepare($sql);
+        $req->bindParam(':id', $id_genre);
+        return $req->fetch();
+    }
+}
--- Models/Genre.php
+++ Models/Genre.php
@@ -1,8 +1,7 @@
-<?php 
+<?php
 
 class Genre extends BDDConnect
 {
-
     public function __construct()
     {
         self::$_pdo = parent::getPdo();
@@ -15,35 +14,4 @@ class Genre extends BDDConnect
         $req->execute();
         return $req->fetchAll();
     }
-
-    public function insertGenre($genre){
-        $sql = "INSERT INTO movie_genres(genre) values (:genre)";
-        $req = self::$_pdo->prepare($sql);
-        $req->bindParam(':genre', $genre);
-        return $req->execute();
-    }
-    
-    public function getGenrebyId($id_genre)
-    {
-        $sql ="SELECT genre FROM movie_genres
-        WHERE id_genre = $id_genre";
-        $req = self::$_pdo->prepare($sql);
-        return $req->execute();
-    }
-
-    public function updateGenre($id_genre, $genre)
-    {
-        $sql ="UPDATE movie_genres
-        SET genre = '$genre'
-        WHERE id_genre = $id_genre";
-        $req = self::$_pdo->prepare($sql);
-    return $req->execute();
-    }
-
-    public function deleteGenre($id_genre)
-    {
-        $sql ="DELETE FROM `movie_genres` WHERE `movie_genres`.`id_genre` = $id_genre";
-        $req = self::$_pdo->prepare($sql);
-    return $req->execute();
-    }
-} 
+}
