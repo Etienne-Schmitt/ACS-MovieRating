1fa66bbf29c848b28540b8b4faa2d77cfb9d4e85
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-02-24 09:24:29 +0100

    Change var for a const

--- assets/js/movieFileInputForm.js
+++ assets/js/movieFileInputForm.js
@@ -76,6 +76,6 @@ function showHiddenDivs(arrayDivs) {
 
 
 $(".custom-file-input").on("change", function () {
-    var fileName = $(this).val().split("\\").pop();
+    const fileName = $(this).val().split("\\").pop();
     $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
