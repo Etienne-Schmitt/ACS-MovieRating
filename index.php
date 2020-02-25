defc3ef8b08dcfe4b053b2e876156502c059a81e
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-02-25 13:30:11 +0100

    Format routes in index.php

--- index.php
+++ index.php
@@ -18,21 +18,8 @@ header('Cache-Control: no-cache');
 
 
 //// GET
-// AdminGenre
-$mainRouter->addRouteGET('/admin/genre/add', 'AdminGenre.showAddGenre');
-$mainRouter->addRouteGET('/admin/genre/edit', 'AdminGenre.showEditGenre');
-$mainRouter->addRouteGET('/admin/genre/delete', 'AdminGenre.showDelGenre');
-// AdminArtist
-$mainRouter->addRouteGET('/admin/artist/add', 'AdminArtist.showAddArtist');
-$mainRouter->addRouteGET('/admin/artist/edit', 'AdminArtist.showEditArtist');
-$mainRouter->addRouteGET('/admin/artist/delete', 'AdminArtist.showDelArtist');
-// AdminMovie
-$mainRouter->addRouteGET('/admin/movie/add', 'AdminMovie.showAddMovie');
-$mainRouter->addRouteGET('/admin/movie/edit', 'AdminMovie.showEditMovie');
-$mainRouter->addRouteGET('/admin/movie/delete', 'AdminMovie.showDelMovie');
-
 // Public
-$mainRouter->addRouteGET('/admin', 'AdminHome.showAdmin');
+$mainRouter->addRouteGET('/admin', 'AdminHome.showHomeAdmin');
 $mainRouter->addRouteGET('/genre', 'Genre.showGenre');
 $mainRouter->addRouteGET('/artist', 'Artist.showArtist');
 $mainRouter->addRouteGET('/director', 'Director.showDirector');
@@ -40,20 +27,38 @@ $mainRouter->addRouteGET('/movie', 'Movie.showMovie');
 $mainRouter->addRouteGET('/', 'Home.showHome');
 $mainRouter->error404GET(":file");
 
+// AdminGenre
+$mainRouter->addRouteGET('/admin/genre/get/:id', 'AdminGenre.getGenreDataJSON');
+$mainRouter->addRouteGET('/admin/genre/add', 'AdminGenre.showAddGenre');
+$mainRouter->addRouteGET('/admin/genre/edit', 'AdminGenre.showEditGenre');
+$mainRouter->addRouteGET('/admin/genre/delete', 'AdminGenre.showDelGenre');
+// AdminArtist
+$mainRouter->addRouteGET('/admin/artist/get/:id', 'AdminArtist.getArtistDataJSON');
+$mainRouter->addRouteGET('/admin/artist/add', 'AdminArtist.showAddArtist');
+$mainRouter->addRouteGET('/admin/artist/edit', 'AdminArtist.showEditArtist');
+$mainRouter->addRouteGET('/admin/artist/delete', 'AdminArtist.showDelArtist');
+// AdminMovie
+$mainRouter->addRouteGET('/admin/movie/get/:id', 'AdminMovie.getMovieDataJSON');
+$mainRouter->addRouteGET('/admin/movie/add', 'AdminMovie.showAddMovie');
+$mainRouter->addRouteGET('/admin/movie/edit', 'AdminMovie.showEditMovie');
+$mainRouter->addRouteGET('/admin/movie/delete', 'AdminMovie.showDelMovie');
+
 
 //// POST
-$mainRouter->addRoutePOST('/admin/genre/add', 'AdminGenre.');
-$mainRouter->addRoutePOST('/admin/genre/edit', 'AdminGenre.');
-$mainRouter->addRoutePOST('/admin/genre/delete', 'AdminGenre.');
+$mainRouter->addRoutePOST('/admin/genre/add', 'AdminGenre.insertNewGenre');
+$mainRouter->addRoutePOST('/admin/genre/edit', 'AdminGenre.editGenre');
+$mainRouter->addRoutePOST('/admin/genre/delete', 'AdminGenre.deleteGenre');
 
-$mainRouter->addRoutePOST('/admin/artist/add', 'AdminArtist.');
-$mainRouter->addRoutePOST('/admin/artist/edit', 'AdminArtist.');
-$mainRouter->addRoutePOST('/admin/artist/delete', 'AdminArtist.');
+$mainRouter->addRoutePOST('/admin/artist/add', 'AdminArtist.insertNewArtist');
+$mainRouter->addRoutePOST('/admin/artist/edit', 'AdminArtist.editArtist');
+$mainRouter->addRoutePOST('/admin/artist/delete', 'AdminArtist.deleteArtist');
 
-$mainRouter->addRoutePOST('/admin/movie/add', 'AdminMovie.');
-$mainRouter->addRoutePOST('/admin/movie/edit', 'AdminMovie.');
-$mainRouter->addRoutePOST('/admin/movie/delete', 'AdminMovie.');
+$mainRouter->addRoutePOST('/admin/movie/add', 'AdminMovie.insertNewMovie');
+$mainRouter->addRoutePOST('/admin/movie/edit', 'AdminMovie.editMovie');
+$mainRouter->addRoutePOST('/admin/movie/delete', 'AdminMovie.deleteMovie');
+//Admin
 
+$mainRouter->addRoutePOST('/admin/login', 'AdminHome.postLogIn');
 
 try {
     $mainRouter->startRouter();
