7f3d1d67cde8e06ed8ccc6a611bf226f4365ca4b
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-02-25 13:46:06 +0100

    Clean everything and check if connect on all admin pages

--- Admin/Controllers/AdminGenreController.php
+++ Admin/Controllers/AdminGenreController.php
@@ -18,23 +18,105 @@ class AdminGenreController extends AdminController
         $this->arrayGenres = Genre::convertGenreArrayForTwig($this->adminGenre->getAllGenres());
     }
 
+    /*
+     ************************
+     *** HTTP method get  ***
+     ************************
+     */
+
     public function showAddGenre()
     {
-        $pageTwig = 'admin/genre/genre.add.admin.html.twig';
-        echo $this->showPage($pageTwig);
-    }
-
-    public function showEditGenre()
-    {
-        $pageTwig = 'admin/genre/genre.edit.admin.html.twig';
-        self::$_twig->addGlobal('arrayGenres', $this->arrayGenres);
-        echo $this->showPage($pageTwig);
+        if ($this->checkIfUserIsLogged()) {
+            $pageTwig = 'admin/genre/genre.add.admin.html.twig';
+            echo $this->showPage($pageTwig);
+        } else {
+            $this->redirectLogin();
+        }
     }
 
     public function showDelGenre()
     {
-        $pageTwig = 'admin/genre/genre.delete.admin.html.twig';
-        self::$_twig->addGlobal('arrayGenres', $this->arrayGenres);
-        echo $this->showPage($pageTwig);
+        if ($this->checkIfUserIsLogged()) {
+            $pageTwig = 'admin/genre/genre.delete.admin.html.twig';
+            self::$_twig->addGlobal('arrayGenres', $this->arrayGenres);
+            echo $this->showPage($pageTwig);
+        } else {
+            $this->redirectLogin();
+        }
+    }
+
+    public function showEditGenre()
+    {
+        if ($this->checkIfUserIsLogged()) {
+            $pageTwig = 'admin/genre/genre.edit.admin.html.twig';
+            self::$_twig->addGlobal('arrayGenres', $this->arrayGenres);
+            echo $this->showPage($pageTwig);
+        } else {
+            $this->redirectLogin();
+        }
+    }
+
+    /*
+     ************************
+     *** HTTP method post ***
+     ************************
+     */
+
+    public function insertNewGenre()
+    {
+        $genre = $_POST['genre-add-new'];
+
+        $this->adminGenre->insertGenre($genre);
+
+        $baseUrl = self::getBaseUrl();
+        header("Location: $baseUrl/admin/genre/add");
+        die();
+    }
+
+    public function editGenre()
+    {
+        $genreOld = $_POST['genre-edit-old'];
+        $genreNew = $_POST['genre-edit-new'];
+
+        $genreID = $this->adminGenre->getGenreId($genreOld);
+        $this->adminGenre->updateGenre($genreID, $genreNew);
+
+        $baseUrl = self::getBaseUrl();
+        header("Location: $baseUrl/admin/genre/edit");
+        die();
+    }
+
+    public function deleteGenre()
+    {
+        $genre = $_POST['genre-delete'];
+
+        $genreID = $this->adminGenre->getGenreId($genre);
+        $test = $this->adminGenre->deleteGenre($genreID);
+
+        $baseUrl = self::getBaseUrl();
+        if (!$test) {
+            //TODO Add error page because the genre is associated with movie
+        }
+        header("Location: $baseUrl/admin/genre/delete");
+        die();
+    }
+
+    /*
+     ************************
+     *** Internal method  ***
+     **********************+*
+     */
+
+    public function getGenreDataJSON($id)
+    {
+        header("Content-Type: application/json");
+        if ($this->adminGenre->genreExist($id)) {
+            $genreData = $this->adminGenre->getGenreById($id);
+
+            echo json_encode($genreData);
+            die();
+        }
+        header("HTTP/1.0 404 Not Found");
+        die();
     }
 }
--- Admin/Controllers/AdminMovieController.php
+++ Admin/Controllers/AdminMovieController.php
@@ -34,25 +34,157 @@ class AdminMovieController extends AdminController
         $this->arrayArtists = AdminArtist::convertArtistArrayForTwig(($this->adminArtist->getAllArtists()));
     }
 
+    /*
+     ************************
+     *** HTTP method get  ***
+     **********************+*
+     */
+
     public function showAddMovie()
     {
-        $pageTwig = 'admin/movie/movie.add.admin.html.twig';
-        echo $this->showPage($pageTwig);
+        if ($this->checkIfUserIsLogged()) {
+            $pageTwig = 'admin/movie/movie.add.admin.html.twig';
+            self::$_twig->addGlobal('arrayGenres', $this->arrayGenres);
+            self::$_twig->addGlobal("arrayArtists", $this->arrayArtists);
+            echo $this->showPage($pageTwig);
+        } else {
+            $this->redirectLogin();
+        }
     }
 
     public function showEditMovie()
     {
-        $pageTwig = 'admin/movie/movie.edit.admin.html.twig';
-        self::$_twig->addGlobal('arrayMovies', $this->arrayMovies);
-        echo $this->showPage($pageTwig);
+        if ($this->checkIfUserIsLogged()) {
+            $pageTwig = 'admin/movie/movie.edit.admin.html.twig';
+            self::$_twig->addGlobal('arrayMovies', $this->arrayMovies);
+            self::$_twig->addGlobal('arrayGenres', $this->arrayGenres);
+            self::$_twig->addGlobal("arrayArtists", $this->arrayArtists);
+            echo $this->showPage($pageTwig);
+        } else {
+            $this->redirectLogin();
+        }
     }
 
     public function showDelMovie()
     {
-        $pageTwig = 'admin/movie/movie.delete.admin.html.twig';
-        self::$_twig->addGlobal('arrayMovies', $this->arrayMovies);
-        echo $this->showPage($pageTwig);
+        if ($this->checkIfUserIsLogged()) {
+            $pageTwig = 'admin/movie/movie.delete.admin.html.twig';
+            self::$_twig->addGlobal('arrayMovies', $this->arrayMovies);
+            echo $this->showPage($pageTwig);
+        } else {
+            $this->redirectLogin();
+        }
     }
 
+    public function getMovieDataJSON($id)
+    {
+        header("Content-Type: application/json");
+        $arrayEnd = [];
+
+        if ($this->adminMovie->movieExist($id)) {
+            $movieData = $this->adminMovie->getMovieById($id);
+
+            $arrayEnd['title'] = $movieData['title'];
+            $arrayEnd['genre'] = $movieData['genre_id'];
+            $arrayEnd['year'] = $movieData['release_year'];
+            $arrayEnd['director'] = $movieData['director_id'];
+            $arrayEnd['poster'] = $movieData['poster'];
+            $arrayEnd['synopsis'] = $movieData['synopsis'];
+
+            echo json_encode($arrayEnd);
+            die();
+        }
+        header("HTTP/1.0 404 Not Found");
+        die();
+    }
+
+    /*
+     ************************
+     *** HTTP method post ***
+     ************************
+     */
+
+    public function insertNewMovie()
+    {
+        $title = $_POST['movie-title-add'];
+        $genreID = $_POST['movie-genre-add'];
+        $directorID = $_POST['movie-director-add'];
+        $year = $_POST['movie-year-add'];
+        $synopsis = $_POST['movie-synopsis-add'];
+
+        $poster = $_FILES['movie-poster-add'];
+
+        if (is_uploaded_file($poster['tmp_name'])) {
+            if (in_array($poster['type'], self::AUTHORIZEDFILESTYPES)) {
+                if (move_uploaded_file($poster['tmp_name'], 'Uploads/posters/' . $poster['name'])) {
+                    if ($this->adminMovie->insertMovie($title, $year, $poster['name'], $synopsis, $genreID, $directorID)) {
+                        $baseUrl = self::getBaseUrl();
+                        header("Location: $baseUrl/admin/movie/add");
+                        die();
+                    }
+                }
+            }
+        }
+        //TODO Add error page
+        $httpVersion = $_SERVER['SERVER_PROTOCOL'];
+        header("$httpVersion 500 Server error");
+        die();
+    }
+
+    public function editMovie()
+    {
+        $movieID = $_POST['movie-title-old'];
+
+        $title = $_POST['movie-title-new'];
+        $genreID = $_POST['movie-genre-new'];
+        $directorID = $_POST['movie-director-new'];
+        $year = $_POST['movie-year-new'];
+        $synopsis = $_POST['movie-synopsis-new'];
+
+        $poster = $_FILES['movie-poster-new'];
+
+        if ($poster['error'] === 4) {
+            $posterName = $this->adminMovie->getMovieById($movieID)['poster'];
+        } else {
+            $posterName = $poster['name'];
+            if (is_uploaded_file($poster['tmp_name'])) {
+                if (in_array($poster['type'], self::AUTHORIZEDFILESTYPES)) {
+                    if (!move_uploaded_file($poster['tmp_name'], 'Uploads/posters/' . $posterName)) {
+                        //TODO Add error page
+                        $httpVersion = $_SERVER['SERVER_PROTOCOL'];
+                        header("$httpVersion 500 Server error");
+                        die();
+                    }
+                }
+            }
+        }
+            if ($this->adminMovie->updateMovie($movieID, $title, $year, $posterName, $synopsis, $genreID, $directorID)) {
+                $baseUrl = self::getBaseUrl();
+                header("Location: $baseUrl/admin/movie/edit");
+                die();
+            }
+        //TODO Add error page
+        $httpVersion = $_SERVER['SERVER_PROTOCOL'];
+        header("$httpVersion 500 Server error");
+        die();
+    }
+
+    public function deleteMovie()
+    {
+        var_dump($_POST);
+
+        $movieID = $_POST['movie-delete'];
+        $test = $this->adminMovie->deleteMovie($movieID);
+
+        if (!$test) {
+            //TODO Add error page because the genre is associated with movie
+            $httpVersion = $_SERVER['SERVER_PROTOCOL'];
+            header("$httpVersion 500 Server error");
+            die();
+        }
+        $baseUrl = self::getBaseUrl();
+        header("Location: $baseUrl/admin/movie/delete");
+        die();
+    }
 
 }
